59ae5fc26e5354ded1242112e866d83f27a1f7b8
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-10-18 20:31:49 -0300

    add Employee class

---
+++ crud-java-oracle/src/br/com/fiap/mercado/view/Employee.java
@@ -0,0 +1,47 @@
+package br.com.fiap.mercado.view;
+
+import java.util.Calendar;
+
+public class Employee {
+	private int code;
+	private String name;
+	private String email;
+	private double wage;
+	private Calendar hiringDate;
+	
+	public Employee() { super(); }
+	
+	public Employee(int code, String name, String email, double wage, Calendar hiringDate) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.email = email;
+		this.wage = wage;
+		this.hiringDate = hiringDate;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public double getWage() {
+		return wage;
+	}
+	public Calendar getHiringDate() {
+		return hiringDate;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setWage(float wage) {
+		this.wage = wage;
+	}
+}
