2b198e237a52f37f4048609e0af48804e762cb35
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-10-21 07:56:22 -0300

    update TestUpdate class

--- crud-java-oracle/src/br/com/fiap/mercado/view/TestUpdate.java
+++ crud-java-oracle/src/br/com/fiap/mercado/view/TestUpdate.java
@@ -4,12 +4,13 @@ public class TestUpdate {
 	public static void main(String[] args) {
 		EmployeeDAO employeeDAO = new EmployeeDAO();
 		
-		Employee employeeToBeUpdated = employeeDAO.fetchById(44);
+		Employee employeeToBeUpdated = employeeDAO.fetchById(3);
+		System.out.println(employeeToBeUpdated.getName());
 		
-		employeeToBeUpdated.setName("pedrimtrembala");
-		employeeToBeUpdated.setEmail("pedro@pedro.com.br");
-		employeeToBeUpdated.setWage(2239.87);
-		
-		employeeDAO.update(employeeToBeUpdated);
+//		employeeToBeUpdated.setName("pedrimtrembala");
+//		employeeToBeUpdated.setEmail("pedro@pedro.com.br");
+//		employeeToBeUpdated.setWage(2239.87);
+//		
+//		employeeDAO.update(employeeToBeUpdated);
 	}
 }
